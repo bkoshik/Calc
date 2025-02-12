95c0953109f0a5b2be5ceb567b9cc82ab6d90e07
Bkoshik <82940786+bkoshik@users.noreply.github.com>
2025-02-12 12:39:05 +0500

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,109 @@
+from math import *
+
+# This vars is first because of errors
+lang = "eng"
+shure = "n"
+con = "y"
+num1 = 0
+changeLang = "Change language (kz/rus/eng) or to exit enter exit: "
+langerror = "Error: Invalid language"
+
+while shure == "n":
+    lang = input(changeLang)
+
+    # Variables in english
+    if lang == "eng":
+        changeLang = "Change language (kz/rus/eng) or to exit enter exit: "
+        num1word = "Enter first number: "
+        num2word = "Enter second number: "
+        opword = "Enter operation (+, -, *, /, ^, sqr): "
+        sumword = "Sum: "
+        diffword = "Difference: "
+        prodword = "Product: "
+        quotword = "Quotient: "
+        powword = "Power: "
+        sqrtword = "Square root: "
+        shureword = "Are you sure? (y/n): "
+        conword = "Continue? (y/n): "
+        zeroerror = "Error: Division by zero"
+        langerror = "Error: Invalid language"
+        operror = "Error: Enter correct operation"
+    # Variables in russian
+    elif lang == "rus":
+        changeLang = "Изменить язык (kz/rus/eng) или чтобы выйти введите exit: "
+        num1word = "Введите первое число: "
+        num2word = "Введите второе число: "
+        opword = "Введите операцию (+, -, *, /, ^, sqr): "
+        sumword = "Сумма: "
+        diffword = "Разность: "
+        prodword = "Произведение: "
+        quotword = "Частное: "
+        powword = "Степень: "
+        sqrtword = "Квадратный корень: "
+        shureword = "Вы уверены? (y/n): "
+        conword = "Продолжить? (y/n): "
+        zeroerror = "Ошибка: Деление на ноль"
+        langerror = "Ошибка: Неверный язык"
+        operror = "Ошибка: Введите правильную операцию"
+    # Variables in kazakh
+    elif lang == "kz":
+        changeLang = "Тілді өзгерту (kz/rus/eng) немесе шығу үшін exit жазыңыз: "
+        num1word = "Бірінші санды енгізіңіз: "
+        num2word = "Екінші санды енгізіңіз: "
+        opword = "Операцияны енгізіңіз (+, -, *, /, ^, sqr): "
+        sumword = "Қосынды: "
+        diffword = "Айырмашылық: "
+        prodword = "Көбейту: "
+        quotword = "Бөліну: "
+        powword = "Дәрежесі: "
+        sqrtword = "Квадрат түбірі: "
+        shureword = "Сіз сенімдісіз бе? (y/n): "
+        conword = "Жалғастыру? (y/n): "
+        zeroerror = "Қате: Нольға бөлу"
+        langerror = "Қате: Дұрыс тілді таңдаңыз"
+        operror = "Қате: Дұрыс операция енгізіңіз"
+    # To exit
+    elif lang == "exit":
+        exit()
+    # If user enters unknown lang
+    if lang != "kz" and lang != "rus" and lang != "eng":
+        print(langerror)
+        continue
+    shure = input(shureword)
+
+# Operations
+while con == "y":
+    # Op is first because of if/else
+    op = input(opword)
+    if op == "sqr":
+        num1 = float(input(num1word))
+    else:
+        num1 = float(input(num1word))
+        num2 = float(input(num2word))
+
+    match op:
+        case "+":
+            print(sumword + str(num1 + num2))
+        case "-":
+            print(diffword + str(num1 - num2))
+        case "*":
+            print(prodword + str(num1 * num2))
+        case "/":
+            if num2 == 0:
+                print(zeroerror)
+                continue
+            else:
+                print(quotword + str(num1 / num2))
+        case "^":
+            print(powword + str(pow(num1, num2)))
+        case "sqr":
+            print(sqrtword + str(sqrt(num1)))
+        case _:
+            print(operror)
+            continue
+    
+    # To ask user
+    con = input(conword)
+
+
+# In plans create calc on C++
